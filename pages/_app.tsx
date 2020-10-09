7e8bf43142eb77464ed6f697aeafd31079b38ff4
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-10-09 01:32:57 -0400

    build: add chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { ThemeProvider } from "@chakra-ui/core"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
 import Head from "next/head"
 
+import { Heading, Link, Icon, Text } from "@chakra-ui/core"
+
 const Home: React.FC = () => {
   return (
     <div>
       <Head>
-        <title>Airtable Admin</title>
+        <title>QCC</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1>Airtable Admin</h1>
+        <Heading as="h1">QCC</Heading>
+
+        <Text>
+          <Link href="https://airtable.com" isExternal>
+            Go to Airtable <Icon name="external-link" mx="2px" />
+          </Link>
+        </Text>
       </main>
     </div>
   )
