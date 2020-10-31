79f2f73396fa6261579e91b5c57d1c6ada572d7e
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-10-31 02:10:16 -0400

    style(route tool): convert Help to Chakra

--- components/EvangelMap/Help.tsx
+++ components/EvangelMap/Help.tsx
@@ -1,3 +1,5 @@
+import { Box, Heading, Link, List, ListItem, Text } from "@chakra-ui/core"
+
 import { useStoreState, useStoreActions } from "../EvangelMap/store"
 
 export const Help: React.FC = () => {
@@ -7,68 +9,92 @@ export const Help: React.FC = () => {
 
   return (
     <>
-      <div className="help">
+      <Box
+        position="absolute"
+        left="1em"
+        bottom="1em"
+        zIndex={1000}
+        backgroundColor="#ffffffdd"
+        boxShadow="0 0 8px #00000033"
+        borderRadius="0.5em"
+        lineHeight={1.4}
+      >
         {isHelpVisible ? (
-          <div className="content">
-            <div className="toggle" onClick={() => hideHelp()}>
-              Close ✕
-            </div>
-            <div className="scrollable">
-              <h2>Airtable Map Viewer for Evangel Deliveries</h2>
+          <Box p="1em" w="22em">
+            <Toggle onClick={() => hideHelp()}>Close ✕</Toggle>
+            <Box maxH="calc(100vh - 10rem - 80px)" overflow="scroll">
+              <Heading as="h2" fontSize="24px">
+                Airtable Map Viewer for Evangel Deliveries
+              </Heading>
 
-              <p>Check out the notes below.</p>
+              <Text>Check out the notes below.</Text>
 
-              <p>
+              <Text>
                 If you still have questions drop em in the the{" "}
-                <a href="https://queensdsa.slack.com/archives/C012THPS340">
-                  #mutualaid-data
-                </a>{" "}
+                {/* TODO: figure out how to color hyperlinks?! */}
+                <Text as="span" textDecoration="underline">
+                  <Link href="https://queensdsa.slack.com/archives/C012THPS340">
+                    #mutualaid-data
+                  </Link>
+                </Text>{" "}
                 channel
-              </p>
+              </Text>
 
-              <h3>What this is</h3>
+              <Heading as="h3" fontSize="18px">
+                What this is
+              </Heading>
 
-              <p>
+              <Text>
                 This map app provides a friendlier view into the Airtable tables
                 we use to manage deliveries.
-              </p>
+              </Text>
 
-              <p>
+              <Text>
                 All <em>updates</em> to driving assignments are still made in
                 the Airtable interface, but this map interface should make it
                 much easier to <em>visualize</em> what drivers are available,
                 where they are coming from, which stops are assigned to them,
                 which stops still need to be assigned, and so on.
-              </p>
+              </Text>
 
-              <h3>How to use this</h3>
+              <Heading as="h3" fontSize="18px">
+                How to use this
+              </Heading>
 
-              <p>There are basically two different ways to get this done:</p>
+              <Text>
+                There are basically two different ways to get this done:
+              </Text>
 
-              <ol>
-                <li>
+              <List as="ol" pl="1em" styleType="decimal" stylePos="outside">
+                <ListItem d="list-item">
                   Keep the Airtable{" "}
-                  <a
-                    target="grid"
-                    href={`https://airtable.com/${process.env.NEXT_PUBLIC_AIRTABLE_RECIPIENTS_TABLE_ID}/${process.env.NEXT_PUBLIC_AIRTABLE_RECIPIENTS_MAP_VIEW_ID}`}
-                  >
-                    grid view for this delivery
-                  </a>{" "}
+                  <Text as="span" textDecoration="underline">
+                    <Link
+                      target="grid"
+                      href={`https://airtable.com/${process.env.NEXT_PUBLIC_AIRTABLE_RECIPIENTS_TABLE_ID}/${process.env.NEXT_PUBLIC_AIRTABLE_RECIPIENTS_MAP_VIEW_ID}`}
+                    >
+                      grid view for this delivery
+                    </Link>
+                  </Text>{" "}
                   open in another window and make assignments row-by-row in the{" "}
                   <strong>Driver</strong> column. Refresh this map view
                   frequently, to see the current state of the assignments.
-                </li>
-                <li>
+                </ListItem>
+                <ListItem>
                   Hover over the map markers to see a link to the associated
                   Airtable record. Click through to open that individual record
                   and assign a driver. (You'll have to scroll down to the{" "}
                   <strong>Driver</strong> field.) Refresh this map view
                   frequently, to see the current state of the assignments.
-                </li>
-              </ol>
+                </ListItem>
+              </List>
 
-              <p>
-                <svg height="20" width="20" style={{ verticalAlign: "middle" }}>
+              <Text>
+                <svg
+                  height="20"
+                  width="20"
+                  style={{ display: "inline", verticalAlign: "middle" }}
+                >
                   <circle
                     cx="10"
                     cy="10"
@@ -81,7 +107,11 @@ export const Help: React.FC = () => {
                 Open rings represent drivers.
                 <br />
                 <br />
-                <svg height="20" width="20" style={{ verticalAlign: "middle" }}>
+                <svg
+                  height="20"
+                  width="20"
+                  style={{ display: "inline", verticalAlign: "middle" }}
+                >
                   <circle
                     cx="10"
                     cy="10"
@@ -93,69 +123,76 @@ export const Help: React.FC = () => {
                 </svg>{" "}
                 Filled discs represent stops. Stops will be gray until they are
                 assigned; then they will be color-coded to match their driver.
-              </p>
+              </Text>
 
-              <p>
+              <Text>
                 Once all assignments are done, use the{" "}
-                <span className="button">Mapquest</span> button to copy a driver
+                <LilButton>Mapquest</LilButton> button to copy a driver
                 itinerary into{" "}
-                <a href="https://www.mapquest.com/routeplanner/copy-paste">
-                  Mapquest's route optimization tool
-                </a>
+                <Text as="span" textDecoration="underline">
+                  <Link
+                    target="mapquest"
+                    href="https://www.mapquest.com/routeplanner/copy-paste"
+                  >
+                    Mapquest's route optimization tool
+                  </Link>
+                </Text>
                 . Be sure to turn on the "Allow us to re-order stops on your
                 route" option before submitting the route, as that's what tells
                 Mapquest to come up with the most time/fuel-efficient route. You
                 can then populate the "Suggested order" column in the{" "}
                 <strong>Delivery Recipients</strong> table to indicate the
                 optimized itinerary.
-              </p>
+              </Text>
 
-              <p>
+              <Text>
                 Once all itineraries are complete, use the{" "}
-                <span className="button">Slack</span> button to copy a driver
-                itinerary for pasting into Slack.
-              </p>
+                <LilButton>Slack</LilButton> button to copy a driver itinerary
+                for pasting into Slack.
+              </Text>
 
-              <h3>Pre-preparation</h3>
+              <Heading as="h3" fontSize="18px">
+                Pre-preparation
+              </Heading>
 
-              <p>
+              <Text>
                 If you are about to do route planning, this has probably already
                 been done for you, but just in case…
-              </p>
+              </Text>
 
-              <p>
+              <Text>
                 These steps need to be performed beforehand, to get the tool set
                 up for the next round of deliveries
-              </p>
+              </Text>
 
               <ul>
-                <li>
+                <ListItem>
                   Change filter on the view{" "}
                   <strong>
                     Volunteers: <em>Current Delivery Drivers Map</em>
                   </strong>{" "}
                   to match the name of the upcoming delivery, e.g. `Evangel -
                   2020-07-07`
-                </li>
+                </ListItem>
 
-                <li>
+                <ListItem>
                   Change filter on the view{" "}
                   <strong>
                     Delivery Recipients: <em>Map</em>
                   </strong>{" "}
                   to match the name of the upcoming delivery, e.g. `Evangel -
                   2020-07-07`
-                </li>
-                <li>
+                </ListItem>
+                <ListItem>
                   Double check that the Map App in{" "}
                   <strong>
                     Maps: Coordinated Deliveries &gt;{" "}
                     <em>Delivery Recipients: Map</em>
                   </strong>{" "}
                   is configured correctly (i.e. pulling from the Map view)
-                </li>
+                </ListItem>
 
-                <li>
+                <ListItem>
                   Double check that the Map App in{" "}
                   <strong>
                     Maps: Coordinated Deliveries &gt;{" "}
@@ -163,61 +200,46 @@ export const Help: React.FC = () => {
                   </strong>{" "}
                   is configured correctly (i.e. pulling from the Current
                   Delivery Drivers Map view)
-                </li>
+                </ListItem>
               </ul>
 
-              <p>
+              <Text>
                 <em>
                   NOTE: Sometimes it seems that simply opening the the Map App
                   configuration is necessary to kick off geocoding of all the
                   recipient records.
                 </em>
-              </p>
-            </div>
-          </div>
+              </Text>
+            </Box>
+          </Box>
         ) : (
-          <div className="toggle" onClick={() => showHelp()}>
-            Need help?
-          </div>
+          <Toggle onClick={() => showHelp()}>Need help?</Toggle>
         )}
-      </div>
-
-      <style jsx>{`
-        .help {
-          position: absolute;
-          left: 1em;
-          bottom: 1em;
-          z-index: 1000;
-          background: #ffffffdd;
-          box-shadow: 0 0 8px #00000033;
-          border-radius: 0.5em;
-          line-height: 1.4;
-        }
-
-        .content {
-          padding: 1em;
-          width: 22em;
-        }
-
-        .toggle {
-          color: #e7212f;
-          padding: 1em;
-          cursor: pointer;
-          text-align: right;
-        }
-
-        .scrollable {
-          max-height: calc(100vh - 10rem - 80px);
-          overflow: scroll;
-        }
-
-        .button {
-          border: solid 1px gray;
-          background: #ffffff33;
-          border-radius: 0.25em;
-          padding: 0 0.25em;
-        }
-      `}</style>
+      </Box>
     </>
   )
 }
+
+const LilButton: React.FC = ({ children }) => (
+  <Text
+    as="span"
+    p="0 0.25em"
+    bg="#ffffff33"
+    border="solid 1px gray"
+    borderRadius="0.25em"
+  >
+    {children}
+  </Text>
+)
+
+const Toggle: React.FC<{ onClick: () => void }> = ({ onClick, children }) => (
+  <Box
+    onClick={onClick}
+    color="#e7212f"
+    p="1em"
+    cursor="pointer"
+    textAlign="right"
+  >
+    {children}
+  </Box>
+)
