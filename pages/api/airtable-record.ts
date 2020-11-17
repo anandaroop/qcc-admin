1d20b0037f4156504472d74073cf4e0a7d9761f6
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-11-16 22:06:26 -0500

    feat: protect airtable-record endpoint with auth session

--- pages/api/airtable-record.ts
+++ pages/api/airtable-record.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import { getSession } from "next-auth/client"
 
 import { airtable } from "../../lib/airtable"
 import { AirtableRecordParams } from "../../types"
@@ -7,24 +8,30 @@ const records = async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-  const {
-    baseId,
-    tableIdOrName,
-    recordId,
-  } = (req.query as unknown) as AirtableRecordParams
+  const session = await getSession({ req })
+  if (session) {
+    const {
+      baseId,
+      tableIdOrName,
+      recordId,
+    } = (req.query as unknown) as AirtableRecordParams
 
-  try {
-    const base: Airtable.Base = airtable.base(
-      baseId || process.env.AIRTABLE_BASE_ID
-    )
-    const record = await base(tableIdOrName as string).find(recordId)
+    try {
+      const base: Airtable.Base = airtable.base(
+        baseId || process.env.AIRTABLE_BASE_ID
+      )
+      const record = await base(tableIdOrName as string).find(recordId)
 
-    res.statusCode = 200
-    res.json(record)
-  } catch (error) {
-    res.statusCode = error.statusCode || 500
-    res.json(error)
+      res.statusCode = 200
+      res.json(record)
+    } catch (error) {
+      res.statusCode = error.statusCode || 500
+      res.json(error)
+    }
+  } else {
+    res.status(401)
   }
+  res.end()
 }
 
 export default records
