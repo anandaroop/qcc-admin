db2e61585cf741c445f0a13af6b69b7d528121e0
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2021-03-15 23:04:08 -0400

    feat(optimizer): one-click update of Airtable suggested ordering

--- components/EvangelMap/RouteOptimizer.tsx
+++ components/EvangelMap/RouteOptimizer.tsx
@@ -3,6 +3,7 @@ import { useStoreState, useStoreActions } from "./store"
 import {
   Box,
   Button,
+  Link,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -12,14 +13,20 @@ import {
   ModalOverlay,
   Spinner,
   Text,
+  useToast,
 } from "@chakra-ui/react"
 import { OptimizedRoute } from "../../lib/optimized-route"
 import { PII } from "../../lib/blurred-pii"
+import { useState } from "react"
+import { useAirtableRecordUpdate } from "../../lib/hooks"
+import { RecipientFields } from "./store/recipients"
 
 // dynamic import to avoid ssr leaflet error
 const RoutingResult = dynamic(() => import("./RoutingResult"), { ssr: false })
 
 export const RouteOptimizer: React.FC = () => {
+  const [isUpdating, setIsUpdating] = useState<boolean>(false)
+  const toast = useToast()
   const { isRouteOptimizerVisible, currentOptimizedRoute } = useStoreState(
     (state) => state.app
   )
@@ -30,6 +37,82 @@ export const RouteOptimizer: React.FC = () => {
   const teardown = () => {
     hideRouteOptimizer()
     clearCurrentOptimizedRoute()
+  }
+
+  const handleUpdate = async () => {
+    try {
+      setIsUpdating(true)
+      const result = await updateAirtableRecords()
+      displaySuccessToast(result.length)
+    } catch (e) {
+      console.error(e)
+      displayErrorToast(e)
+    } finally {
+      setIsUpdating(false)
+      teardown()
+    }
+  }
+
+  const updateAirtableRecords = () => {
+    const promisedUpdates = currentOptimizedRoute.orderedRecipients.map(
+      (recipient, i) => {
+        const { updateRecord } = useAirtableRecordUpdate<RecipientFields>({
+          tableIdOrName: "Delivery Recipients", // TODO: id?
+          recordId: recipient.id as string,
+        })
+
+        return updateRecord({
+          "Suggested order": i + 1,
+        })
+      }
+    )
+
+    return Promise.all(promisedUpdates)
+  }
+
+  const displaySuccessToast = (updateCount: number) => {
+    toast({
+      position: "top",
+      duration: null,
+      title: "Nice!",
+      description: (
+        <div>
+          Updated the suggested order for {updateCount} stops. At this point you
+          should{" "}
+          <Link
+            textDecoration="underline"
+            onClick={() => window.location.reload()}
+          >
+            refresh the window
+          </Link>{" "}
+          to see the updated results
+        </div>
+      ),
+      status: "success",
+    })
+  }
+
+  const displayErrorToast = (e: Error) => {
+    toast({
+      position: "top",
+      duration: null,
+      title: "Uh-oh",
+      description: (
+        <div>
+          Seems like that didn't work.
+          {e.message && <p>{e.message}</p>}
+          You can{" "}
+          <Link
+            textDecoration="underline"
+            onClick={() => window.location.reload()}
+          >
+            refresh the window
+          </Link>{" "}
+          and try again, to see if that helps.
+        </div>
+      ),
+      status: "error",
+    })
   }
 
   return (
@@ -64,10 +147,16 @@ export const RouteOptimizer: React.FC = () => {
           </ModalBody>
         )}
         <ModalFooter>
-          {/* <Button variant="ghost" onClick={teardown}> Cancel </Button> */}
-          <Button colorScheme="blue" mr={3} onClick={teardown}>
-            Got it
+          <Button variant="ghost" onClick={teardown} mr={3}>
+            Cancel
           </Button>
+
+          {currentOptimizedRoute && (
+            <Button colorScheme="blue" mr={3} onClick={handleUpdate}>
+              Looks good, update Airtable for me
+              {isUpdating && <Spinner ml={2} />}
+            </Button>
+          )}
         </ModalFooter>
       </ModalContent>
     </Modal>
