b36c84e5f949964d4c2c3e4591f28a1caf7bb8e7
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2021-09-05 00:54:09 -0400

    feat: support PII in title component

--- lib/blurred-pii/index.ts
+++ lib/blurred-pii/index.ts
@@ -1,3 +1,4 @@
 export { BlurredPIIProvider } from "./components/BlurredPIIProvider"
 export { PII } from "./components/PII"
 export { useBlurredPII } from "./hook"
+export { BlurredPIIContext } from "./context/BlurredPIIContext"
